9abda380c7eca8dfbb6a942d66c902c1973e5535
IlyaKokorev <kockorev.ilya@gmail.com>
2021-03-01 13:31:17 +0300

    added help field and hash

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -30,11 +30,64 @@ RSpec.describe GameQuestion, type: :model do
       expect(game_question.text).to eq(game_question.question.text)
       expect(game_question.level).to eq(game_question.question.level)
     end
+
+    it 'correct .help_hash' do
+      # на фабрике у нас изначально хэш пустой
+      expect(game_question.help_hash).to eq({})
+
+      # добавляем пару ключей
+      game_question.help_hash[:some_key1] = 'blabla1'
+      game_question.help_hash['some_key2'] = 'blabla2'
+
+      # сохраняем модель и ожидаем сохранения хорошего
+      expect(game_question.save).to be_truthy
+
+      # загрузим этот же вопрос из базы для чистоты эксперимента
+      gq = GameQuestion.find(game_question.id)
+
+      # проверяем новые значение хэша
+      expect(gq.help_hash).to eq({some_key1: 'blabla1', 'some_key2' => 'blabla2'})
+    end
   end
 
   context 'correct .correct_answer_key' do
     it 'be correct' do
       expect(game_question.correct_answer_key). to eq('b')
     end
+  end
+
+  context 'user helpers' do
+    it 'correct audience_help' do
+      # Проверяем, что объект не включает эту подсказку
+      expect(game_question.help_hash).not_to include(:audience_help)
+
+      # Добавили подсказку. Этот метод реализуем в модели
+      # GameQuestion
+      game_question.add_audience_help
+
+      # Ожидаем, что в хеше появилась подсказка
+      expect(game_question.help_hash).to include(:audience_help)
+
+      # Дёргаем хеш
+      ah = game_question.help_hash[:audience_help]
+      # Проверяем, что входят только ключи a, b, c, d
+      expect(ah.keys).to contain_exactly('a', 'b', 'c', 'd')
+    end
+  end
+
+  it 'correct fifty_fifty' do
+    # сначала убедимся, в подсказках пока нет нужного ключа
+    expect(game_question.help_hash).not_to include(:fifty_fifty)
+    # вызовем подсказку
+    game_question.add_audience_help
+
+    game_question.add_fifty_fifty
+
+    # проверим создание подсказки
+    expect(game_question.help_hash).to include(:fifty_fifty)
+    ff = game_question.help_hash[:fifty_fifty]
+
+    expect(ff).to include('b') # должен остаться правильный вариант
+    expect(ff.size).to eq 2 # всего должно остаться 2 варианта
   end
 end
